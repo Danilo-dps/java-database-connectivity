be2d8e02743165f9e5e6c24c6e7378d47842e2e9
danilo-dps <danilodpsnilo@outlook.com>
2024-10-11 00:37:45 -0300

    Preparação do primeiro projeto no Eclipse

---
+++ jdbc1/src/application/Program.java
@@ -0,0 +1,15 @@
+package application;
+
+import java.sql.Connection;
+
+import db.DB;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		@SuppressWarnings("unused")
+		Connection conn = DB.getConnection();
+		DB.closeConnection();
+	}
+}
